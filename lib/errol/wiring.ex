ad3e51df95a31d4212a0b2c2116094d78e38dd81
uesteibar <uesteibar@gmail.com>
2018-04-06 16:31:40 +0200

    feat(wiring): Add after, before and error pipes

--- lib/errol/wiring.ex
+++ lib/errol/wiring.ex
@@ -3,7 +3,10 @@ defmodule Errol.Wiring do
     quote do
       import unquote(__MODULE__)
       Module.register_attribute(__MODULE__, :wirings, accumulate: true)
-      Module.register_attribute(__MODULE__, :connection, acumulate: false)
+      Module.register_attribute(__MODULE__, :pipe_before, accumulate: true)
+      Module.register_attribute(__MODULE__, :pipe_after, accumulate: true)
+      Module.register_attribute(__MODULE__, :pipe_error, accumulate: true)
+      Module.register_attribute(__MODULE__, :connection, [])
       @connection []
       @before_compile unquote(__MODULE__)
 
@@ -12,6 +15,24 @@ defmodule Errol.Wiring do
       def start_link(_) do
         Supervisor.start_link(__MODULE__, nil, name: __MODULE__)
       end
+    end
+  end
+
+  defmacro pipe_before(callback) do
+    quote bind_quoted: [callback: callback] do
+      @pipe_before callback
+    end
+  end
+
+  defmacro pipe_after(callback) do
+    quote bind_quoted: [callback: callback] do
+      @pipe_after callback
+    end
+  end
+
+  defmacro pipe_error(callback) do
+    quote bind_quoted: [callback: callback] do
+      @pipe_error callback
     end
   end
 
@@ -26,7 +47,8 @@ defmodule Errol.Wiring do
       def init(_) do
         {:ok, connection} = AMQP.Connection.open(@connection)
 
-        Enum.map(@wirings, fn {queue, routing_key, callback} ->
+        @wirings
+        |> Enum.map(fn {queue, routing_key, callback} ->
           Supervisor.child_spec(
             {
               Errol.Consumer.Server,
@@ -36,6 +58,9 @@ defmodule Errol.Wiring do
                 routing_key: routing_key,
                 connection: connection,
                 callback: callback,
+                pipe_before: @pipe_before,
+                pipe_after: @pipe_after,
+                pipe_error: @pipe_error,
                 exchange: {@exchange, @exchange_type}
               ]
             },
--- test/errol/wiring_test.exs
+++ test/errol/wiring_test.exs
@@ -9,11 +9,21 @@ defmodule Errol.WiringTest do
     def consume_all(_message), do: :ok
   end
 
+  defmodule Middleware do
+    def run_before(_message), do: :ok
+    def run_after(_message), do: :ok
+    def run_error(_message), do: :ok
+  end
+
   defmodule TestWiring do
     use Wiring
 
     @exchange "wiring_exchange"
     @exchange_type :topic
+
+    pipe_before(&Middleware.run_before/1)
+    pipe_after(&Middleware.run_after/1)
+    pipe_error(&Middleware.run_error/1)
 
     consume("message_success", "message.success", &Consumer.consume_success/1)
     consume("message_all", "message.*", &Consumer.consume_all/1)
@@ -37,8 +47,15 @@ defmodule Errol.WiringTest do
       assert %{
                queue: "message_success",
                routing_key: "message.success",
-               exchange: "wiring_exchange"
+               exchange: "wiring_exchange",
+               pipe_before: [before_callback],
+               pipe_after: [after_callback],
+               pipe_error: [error_callback]
              } = GenServer.call(:message_success_consumer, :config)
+
+      assert before_callback == (&Errol.WiringTest.Middleware.run_before/1)
+      assert after_callback == (&Errol.WiringTest.Middleware.run_after/1)
+      assert error_callback == (&Errol.WiringTest.Middleware.run_error/1)
 
       assert %{queue: "message_all", routing_key: "message.*", exchange: "wiring_exchange"} =
                GenServer.call(:message_all_consumer, :config)
