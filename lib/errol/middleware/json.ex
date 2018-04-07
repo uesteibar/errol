e57ea48f161fa10830082d9eb64db655c3f58aac
uesteibar <uesteibar@gmail.com>
2018-04-07 18:53:13 +0200

    docs(middleware): Add typespecs for json middleware

--- lib/errol/middleware/json.ex
+++ lib/errol/middleware/json.ex
@@ -1,6 +1,7 @@
 defmodule Errol.Middleware.Json do
   alias Errol.Message
 
+  @spec parse(Message.t()) :: {:ok, Message.t()} | {:error, reason :: any()}
   def parse(%Message{payload: payload} = message) do
     case Jason.decode(payload) do
       {:ok, parsed_payload} ->
