801ea9652babc75327e192888630264b90768669
uesteibar <uesteibar@gmail.com>
2018-04-09 01:13:06 +0200

    docs(ex_doc): Add and configure ex_doc

--- mix.exs
+++ mix.exs
@@ -1,14 +1,21 @@
 defmodule Errol.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @github "https://github.com/uesteibar/errol"
+
   def project do
     [
       app: :errol,
-      version: "0.1.0",
-      elixir: "~> 1.6",
+      version: @version,
+      elixir: "~> 1.5",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
+      package: package(),
+      docs: docs(),
+      source_url: @github,
       test_coverage: [tool: Coverex.Task]
     ]
   end
@@ -26,7 +33,30 @@ defmodule Errol.MixProject do
       {:amqp, "~> 1.0"},
       {:jason, "~> 1.0", optional: true},
       {:coverex, "~> 1.4", only: :test},
-      {:mock, "~> 0.3.0", only: :test}
+      {:mock, "~> 0.3.0", only: :test},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      files: [
+        "lib",
+        "mix.exs",
+        "README.md",
+        "LICENSE"
+      ],
+      links: %{"github" => @github},
+      maintainers: ["Unai Esteibar <uesteibar@gmail.com>"],
+      licenses: ["MIT"]
+    ]
+  end
+
+  defp docs do
+    [
+      source_ref: "v#{@version}",
+      main: "readme",
+      extras: ["README.md"]
     ]
   end
 
